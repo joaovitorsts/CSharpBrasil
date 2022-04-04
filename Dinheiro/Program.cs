bfae39e748a3ce6f5c2a6e9bcbea17b918ee93f1
João Vitor dos Santos <jsantos@isolutions.com.br>
2022-04-04 16:34:33 -0300

    Manipulação de dinheiro

---
+++ Dinheiro/Program.cs
@@ -0,0 +1,27 @@
+﻿using Caelum.Stella.CSharp.Vault;
+using System.Diagnostics;
+using System.Globalization;
+
+Money money = 10.00;
+Debug.WriteLine(money);
+
+double valor1 = 10.00;
+double valor2 = 20.00;
+Money total = valor1 + valor2;
+
+Debug.WriteLine(total);
+
+decimal minuendo = 20;
+decimal subtraindo = 15;
+Money diferenca = minuendo - subtraindo;
+Debug.WriteLine(diferenca);
+
+Money euro = new Money(Currency.EUR, 1000);
+Debug.WriteLine(euro);
+
+Money dolar = new Money(Currency.USD, 1000);
+Debug.WriteLine(dolar);
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+Debug.WriteLine(dolar);
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
